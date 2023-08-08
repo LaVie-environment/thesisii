b373a460a2b353f038c39e802139f76e49995c8a
armzig <bellotaophyc@outlook.com>
2023-08-08 07:45:25 +0100

    figuring why i cannot update the ec2 instance

--- main.tf
+++ main.tf
@@ -15,24 +15,31 @@ provider "aws" {
 
 
 resource "aws_key_pair" "ack-key-pair" {
-    key_name   = "ack-key-pair"
-    public_key = tls_private_key.rsa.public_key_openssh
+    key_name   = var.key_pair_name
+    public_key = tls_private_key.ack_key.public_key_openssh
+
+    provisioner "local-exec"{
+    command = "echo '${tls_private_key.ack_key.private_key_pem}' > ./${var.key_pair_name}.pem"
+  }
+
 }
 
-resource "tls_private_key" "rsa" {
+resource "tls_private_key" "ack_key" {
     algorithm = "RSA"
     rsa_bits  = 4096
 }
 
+/*
 resource "local_file" "ack-key" {
     content  = tls_private_key.rsa.private_key_pem
     filename = "ack-key-pair"
 }
+*/
 
 resource "aws_instance" "ack-env" {
     ami = "ami-09dd2e08d601bff67"
     instance_type = "t2.micro"
-    key_name = "ack-key-pair"
+    key_name = aws_key_pair.ack-key-pair.key_name
     vpc_security_group_ids = [aws_security_group.ack-env-wall.id]
     
     tags = {
